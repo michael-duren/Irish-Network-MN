a2b510d046105883d6a77b10c508d3267de7e1d7
Michael Duren <michaeld@michaelduren.com>
2023-02-06 10:28:16 -0600

    edit mobile nav, add _document

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   };
 
   return (
-    <header className="top-0 left-0 m-6 flex h-[10vh] min-h-[10rem]  items-center justify-between ">
+    <header className="sticky top-0 left-0 my-20 flex h-[10vh] min-h-[10rem] items-center justify-between  bg-white  ">
       {/* Logo */}
       <div className="relative h-40 w-40">
         <Link className="" href="/">
@@ -33,19 +33,19 @@ export default function Header() {
       </div>
       {/* Nav Links */}
 
-      {/* Desktop Nav */}
-      <nav>
+      <nav className="bg-white">
         <ul
-          className={`absolute top-[150px]  flex w-[300px] flex-col space-y-8  rounded-md border-2 border-gray-300 bg-white p-8  shadow-md transition-all duration-200 ease-in md:static md:z-auto md:my-0 md:mr-8 md:w-auto md:flex-row  md:space-y-0 md:border-none md:p-0 md:shadow-none ${
-            isMenuOpen ? "right-10" : "right-[-300px]"
+          className={`absolute left-0 z-[-1]  flex w-[100%]  flex-col space-y-8  rounded-md border-2 border-gray-300 bg-white p-8 shadow-md transition-all duration-500 ease-in md:static md:z-auto md:my-0 md:mr-8 md:w-auto md:flex-row md:space-x-8 md:space-y-0 md:border-none md:p-0 md:shadow-none ${
+            isMenuOpen ? "top-[100%]" : "top-[-500%]"
           } `}
         >
+          {/* Nav Link */}
           {links.map((link) => {
             return (
               <li
                 key={link.name}
-                className={`mx-2 p-2 text-xl  font-thin duration-300 hover:text-red-600  md:text-xl lg:text-2xl ${
-                  currentRoute === link.link ? "text-red-700" : "text-black"
+                className={`mx-2 p-2 text-xl font-extralight  duration-500 hover:text-red-600  md:text-xl ${
+                  currentRoute === link.link ? "text-red-700" : "text-gray-900"
                 }`}
               >
                 <Link onClick={onClickHandler} href={link.link}>
@@ -58,7 +58,7 @@ export default function Header() {
         {/* Hamburger */}
         <div
           onClick={onClickHandler}
-          className="absolute top-16 right-16 flex w-[20rem]  cursor-pointer justify-end text-gray-400 md:hidden"
+          className="absolute top-20 right-16 flex w-[20rem]  cursor-pointer justify-end text-gray-400 md:hidden"
         >
           {isMenuOpen ? (
             <IoMdClose className="h-10 w-10 " />
