ae18fd6e3a4040f800f74eaaf56408249121248e
Michael Duren <michaeld@michaelduren.com>
2023-02-12 09:07:02 -0600

    finished styling of writeformevent and modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,15 +1,16 @@
 import { Fragment, type ReactNode } from "react";
 import type { Dispatch, SetStateAction } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { IoMdClose } from "react-icons/io";
 
 type ModalProps = {
   isOpen: boolean;
   closeModal: Dispatch<SetStateAction<boolean>>;
   children?: ReactNode;
-  title?: string;
+  title: string;
 };
 
-const Modal = ({ isOpen, closeModal, children }: ModalProps) => {
+const Modal = ({ isOpen, closeModal, children, title }: ModalProps) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={closeModal}>
@@ -22,11 +23,11 @@ const Modal = ({ isOpen, closeModal, children }: ModalProps) => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
+          <div className="fixed  inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
 
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+        <div className="fixed inset-0 items-center overflow-y-auto">
+          <div className="flex min-h-full  items-center justify-center p-4 text-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -36,31 +37,21 @@ const Modal = ({ isOpen, closeModal, children }: ModalProps) => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className="w-full min-w-[30rem] max-w-[90vw]  transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <div
+                  onClick={() => closeModal(false)}
+                  className="flex w-full items-center justify-end"
+                >
+                  <IoMdClose className="cursor-pointer text-2xl text-gray-400 hover:text-gray-700" />
+                </div>
                 <Dialog.Title
                   as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
+                  className="text-left text-lg font-medium leading-6 text-gray-900"
                 >
-                  Add event
+                  {title}
                 </Dialog.Title>
-                <div className="mt-2">{children}</div>
 
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
-                    onClick={() => closeModal(false)}
-                  >
-                    Add Event
-                  </button>
-                  <button
-                    type="button"
-                    className="mx-8 inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
-                    onClick={() => closeModal(false)}
-                  >
-                    Cancel
-                  </button>
-                </div>
+                <div className="mt-2 flex flex-col">{children}</div>
               </Dialog.Panel>
             </Transition.Child>
           </div>
--- src/components/WriteEventForm/index.tsx
+++ src/components/WriteEventForm/index.tsx
@@ -24,16 +24,141 @@ type WriteEventFormProps = {
 
 const WriteEventForm = ({ isOpen, closeModal }: WriteEventFormProps) => {
   return (
-    <Modal isOpen={isOpen} closeModal={closeModal}>
-      <form>
-        <input
-          type="text"
-          id="shortDescription"
-          className="h-full w-full rounded-xl border border-gray-300 p-3 outline-none focus:border-gray-600"
-          placeholder="Short Description"
-        />
-      </form>
-    </Modal>
+    <div>
+      <Modal isOpen={isOpen} title={"Add Event"} closeModal={closeModal}>
+        <form className="my-8 mx-4 flex flex-col justify-center space-y-8">
+          {/* Title Date */}
+          <div className=" flex items-center justify-around space-x-4">
+            <div className="flex flex-1 flex-col items-start justify-center space-x-4 space-y-4">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                className=" w-full rounded-xl  border border-gray-300 p-3 outline-none focus:border-gray-600"
+                placeholder="Title"
+              />
+            </div>
+            <div className="flex flex-1 flex-col items-start justify-center space-x-4 space-y-4">
+              <label htmlFor="date">Date</label>
+              <input
+                className=" w-full rounded-xl  border border-gray-300 p-3 outline-none focus:border-gray-600"
+                id="date"
+                type={"text"}
+                placeholder="DD/MM/YY"
+              />
+            </div>
+          </div>
+          {/* Location Address */}
+          <div className="flex flex-col items-center justify-around space-x-4 md:flex-row">
+            <div className="flex w-full flex-1 flex-col items-start justify-center space-x-4 space-y-4">
+              <label htmlFor="location">Location</label>
+              <input
+                type="text"
+                id="location"
+                className=" w-full rounded-xl  border border-gray-300 p-3 outline-none focus:border-gray-600"
+                placeholder="Dunn Bros"
+              />
+            </div>
+            <div className="flex w-full flex-1 flex-col items-start justify-center space-x-4 space-y-4">
+              <label htmlFor="date">Address</label>
+              <input
+                className="w-full rounded-xl  border border-gray-300 p-3 text-xs outline-none focus:border-gray-600 md:text-base"
+                id="date"
+                type={"text"}
+                placeholder="1569 Grand Ave, St Paul, MN 55105"
+              />
+            </div>
+          </div>
+          {/* price, ticket link */}
+          <div className="flex flex-col items-center justify-around space-x-8 md:flex-row">
+            <div className="flex w-full flex-1 flex-col items-start justify-center space-x-4 space-y-4">
+              <label htmlFor="location">Price</label>
+              <input
+                type="text"
+                id="location"
+                className=" w-full rounded-xl  border border-gray-300 p-3 outline-none focus:border-gray-600"
+                placeholder="25.00"
+              />
+            </div>
+            <div className="flex w-full flex-1 flex-col items-start justify-center space-x-4 space-y-4">
+              <label htmlFor="date">Ticket Link</label>
+              <input
+                className="w-full rounded-xl  border border-gray-300 p-3 text-xs outline-none focus:border-gray-600 md:text-base"
+                id="date"
+                type={"text"}
+                placeholder="https://www.eventbrite.com/"
+              />
+            </div>
+          </div>
+          {/* image and register */}
+          <div className="flex flex-col items-center justify-around space-x-8 md:flex-row">
+            <div className="flex w-full flex-col items-start justify-center space-x-4 space-y-4">
+              <label htmlFor="location">Image</label>
+              <input
+                type="text"
+                id="location"
+                className=" w-full rounded-xl  border border-gray-300 p-3 outline-none focus:border-gray-600"
+                placeholder=""
+              />
+            </div>
+            <div className="flex flex-col items-start justify-start space-x-4 space-y-4">
+              <label htmlFor="date">Attendees must register?</label>
+              <input
+                className="h-4 w-4 rounded  border  border-gray-300 p-4 text-lg outline-none focus:border-gray-600 md:text-base"
+                id="date"
+                type={"checkbox"}
+              />
+            </div>
+          </div>
+
+          {/* Event Summary */}
+          <div className="space-y-4">
+            <label htmlFor="excerpt">Event Summary</label>
+            <textarea
+              id="excerpt"
+              className="h-full w-full rounded-xl border border-gray-300 p-3 outline-none focus:border-gray-600"
+              placeholder="Event Summary"
+            />
+          </div>
+          {/* Description */}
+          <div className="space-y-4">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              className="h-full w-full rounded-xl border border-gray-300 p-3 outline-none focus:border-gray-600"
+              placeholder="Event Details"
+              rows={10}
+            />
+          </div>
+          {/* additionalInformation */}
+          <div className="space-y-4">
+            <label htmlFor="additionalInformation">Addiontal Information</label>
+            <input
+              type="text"
+              id="additionalInformation"
+              className="h-full w-full rounded-xl border border-gray-300 p-3 outline-none focus:border-gray-600"
+              placeholder="..."
+            />
+          </div>
+          {/* Buttons */}
+          <div>
+            <button
+              type="submit"
+              className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+            >
+              Add Event
+            </button>
+            <button
+              type="button"
+              className="mx-8 inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+              onClick={() => closeModal(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </Modal>
+    </div>
   );
 };
 
