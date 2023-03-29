4229c16a887ac636336c90f60d798eb69f4c4033
Michael Duren <michaeld@michaelduren.com>
2023-03-29 15:31:02 -0500

    addressed build error

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ const Custom404 = () => {
   return (
     <div className="flex h-[60vh] w-full items-start justify-center">
       <div className="mx-4 mt-12 flex flex-col items-center space-y-8">
-        <h1 className="text-3xl">404 - We're Sorry This Page Doesn't Exist 😢</h1>
+        <h1 className="text-3xl">404 - We&apos;re Sorry This Page Doesn't Exist 😢</h1>
         <div>Please check the URL you entered and try again.</div>
       </div>
     </div>
